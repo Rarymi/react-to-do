ffe98bd452bb5548d50bc002082fac1e4cdb7e86
Amanda Rarymi <amanda.rarymi@gmail.com>
2021-10-21 22:33:11 -0300

    🔨 Deploy stuffs

--- src/modules/shared/services/api/BaseService.js
+++ src/modules/shared/services/api/BaseService.js
@@ -3,6 +3,6 @@ import HttpClient from '../HttpClient';
 export default class BaseService {
   constructor(baseHref) {
     this.baseHref = baseHref;
-    this.httpClient = new HttpClient('http://localhost:8000/');
+    this.httpClient = new HttpClient('https://amanda-react-to-do-backend.herokuapp.com/');
   }
 }
